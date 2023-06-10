52210a848243e7329fbee2d9a8410479866b2f7f
Juan Solar <juansolarsolar@yahoo.com>
2023-06-10 17:01:06 -0400

    reloj-terminado

---
+++ src/Counter.jsx
@@ -0,0 +1,90 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+
+const SecondsCounter = (props) => {
+  return (
+    <>
+      <div className="container">
+        <div className="container-sm ceros   border rounded  ">
+        <i className="bi bi-clock" ></i></div>
+        <div className="container-lg ceros  border rounded  ">
+          {props.counterCinco % 10}
+        </div>
+        <div className="container-lg ceros  border rounded  ">
+          {props.counterCuatro % 10}
+        </div>
+        <div className="container-xl ceros  border rounded  ">
+          {props.counterTres % 10}
+        </div>
+        <div className="container-xl ceros border rounded  ">
+          {props.counterDos % 10}
+        </div>
+        <div className="container-xl ceros  border rounded  ">
+          {props.counterUno % 10}
+        </div>
+      </div>
+    </>
+  );
+};
+// import SecondsCounter from './Counter';
+export default SecondsCounter;
+
+let counter = 0;
+let counterUno = 0;
+let counterDos = 0;
+let counterTres = 0;
+let counterCuatro = 0;
+let counterCinco = 0;
+
+
+setInterval(() => {
+  counterUno = Math.floor(counter / 1);
+  counterDos = Math.floor(counter / 10);
+  counterTres = Math.floor(counter / 100);
+  counterCuatro = Math.floor(counter / 1000);
+  counterCinco = Math.floor(counter / 10000);
+
+ReactDOM.render(
+  <SecondsCounter
+    counterUno={counterUno}
+    counterDos={counterDos}
+    counterTres={counterTres}
+    counterCuatro={counterCuatro}
+    counterCinco={counterCinco}
+    // countersix={countersix}
+  />,
+  document.querySelector("#root")
+  );
+  console.log(counterUno, counterDos, counterTres, counterCuatro, counterCinco)
+  counter++;
+}, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+// setInterval(function () {
+//   counterCinco = { counterCinco };
+//   counterCuatro = { counterCuatro };
+//   counterTres = { counterTres };
+//   counterDos = { counterDos };
+//   counterUno = { counterUno };
+
+//   counterCinco = Math.floor(counter / 100000);
+//   counterCuatro = Math.floor(counter / 10000);
+//   counterTres = Math.floor(counter / 1000);
+//   counterDos = Math.floor(counter / 100);
+//   counterUno = Math.floor(counter/ 10);
+  
+//   counter++;
+// }, 1000);
